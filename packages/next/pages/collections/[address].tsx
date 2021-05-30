50be4f9890261140317d70e52993d6022bebc560
Entreprenerd <valeranijr@gmail.com>
2021-05-30 17:06:27 +0200

    fix: compilation

---
+++ packages/next/pages/collections/[address].tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import { useRouter } from "next/dist/client/router";
+import NFT from "../../components/NFT";
+import { CentredDeadline } from "../all";
+import { Container } from "../view/[dropId]";
+import { Title } from "../redeem/[dropId]";
+import useMyClaims from "../../hooks/useMyClaims";
+import { Claim } from "../../types";
+
+// If time allows
+const MyCollectionPage: React.FC = () => {
+  const router = useRouter();
+  const { address } = router.query;
+
+  const drops = useMyClaims(String(address));
+  return (
+    <Container>
+      <Title>{address} Collection</Title>
+      {drops?.map((claim: Claim) => (
+        <NFT image={claim.drop.tokenURI} />
+      ))}
+    </Container>
+  );
+};
+
+export default MyCollectionPage;
+
+const Address = styled(CentredDeadline)`
+  margin: 5px;
+`;
